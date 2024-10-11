135b53c076e9fdd8929d1c11ecd330a40f869d23
zlatapolikova <poliakova.zlata@student.uzhnu.edu.ua>
2024-10-12 00:14:33 +0300

    Format code

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,9 @@
-import React from "react";
-import Header from "./Header";
-import Footer from "./Footer";
-import "./App.css";
+import './App.css';
+
+import React from 'react';
+
+import Footer from './Footer';
+import Header from './Header';
 
 const App = () => {
   return (
